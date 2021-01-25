ce450f2edf996704e89bbe15bcfcfdb188c2454e
SophieSabat <sonyasab2000@gmail.com>
2021-01-25 23:19:33 +0200

    task 1

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {users} from '../../database/usersDB';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users = users;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
