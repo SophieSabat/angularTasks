5ef2ce5d64566041bffd4168d0617fa48f8f69d8
SophieSabat <sonyasab2000@gmail.com>
2021-02-05 19:09:10 +0200

    task 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import {HttpClientModule} from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FullUserComponent } from './components/full-user/full-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
-    PostComponent,
+    UsersComponent,
+    UserComponent,
+    HomeComponent,
+    FullUserComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    RouterModule.forRoot([
+      {
+        path: 'link/home', component: HomeComponent
+      },
+      {
+        path: 'link/users', component: UsersComponent, children: [
+          {path: ':id', component: FullUserComponent}
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
